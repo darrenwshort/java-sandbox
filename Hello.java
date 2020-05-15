a2b8ce04a16da68487378dffa19df81b82206536
darrenwshort <dwshort707@gmail.com>
2020-05-15 01:39:34 -0700

    initial commit

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+  public static void main(String[] args){
+    // this is our main method, where we create teh starting point for our program
+    System.out.println("Hello World!");
+	}
+}
